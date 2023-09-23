efe473bfd1bb65389b581febe0ae636e68a4618d
akshatp9454 <akshatp9454@gmail.com>
2023-09-23 11:00:26 +0530

    pairs added

---
+++ my-codes/pairs.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // pair<int, int> p = {1, 3};
+    // cout<<p.first<<" "<<p.second;
+
+    // pair<int, pair<int, int>> p = {1, {3, 4}};
+    // cout<<p.first<<" "<<p.second.first<< " "<<p.second.second;
+    
+    pair<int,int> arr[] = {{1, 2}, {2, 5}, {5, 1}};
+    cout<<arr[1].second;
+    
+    return 0;
+}
